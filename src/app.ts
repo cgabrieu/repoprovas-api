27127a80caaf9233f6f1fd86bd362d55330925f8
Carlos Gabriel <carlosgabrielpc@live.com>
2021-12-16 23:37:16 -0300

    feat: prepare db connection with typeorm

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,22 @@
 import express from 'express';
 import cors from 'cors';
+import 'reflect-metadata';
 import router from './router';
 import errorMiddleware from './middlewares/errorMiddleware';
+import connectDatabase from "./connection/database";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use(router);
 app.use(errorMiddleware);
 
-export async function init () {
-    await connectDatabase();
-  }
+
+
+export async function init() {
+  await connectDatabase();
+}
 
 export default app;
