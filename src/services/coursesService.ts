573846e2988d214b636bd3028457f1ebd6a749a4
Carlos Gabriel <carlosgabrielpc@live.com>
2021-12-18 15:48:32 -0300

    fix: update craete and getlist services

--- src/services/coursesService.ts
+++ src/services/coursesService.ts
@@ -6,20 +6,20 @@ import Course from '../protocols/Course';
 export async function create(courseBody: Course): Promise<CourseEntity> {
   const { name } = courseBody;
 
-  const existsCourse = await getRepository('courses').findOne({ name });
+  const existsCourse = await getRepository(CourseEntity).findOne({ name });
   if (existsCourse) {
     throw new Conflict('Course already registered.');
   }
 
   const course = new CourseEntity();
   course.name = name;
-  await getRepository('courses').save(course);
+  await getRepository(CourseEntity).save(course);
 
   return course;
 }
 
-export async function getList(): Promise<CourseEntity[]> {
-  const courses: CourseEntity[] = await getRepository('courses').find();
+export async function getList(): Promise<Course[]> {
+  const courses: Course[] = await getRepository(CourseEntity).find();
 
  return courses;
 }
