b9b5143fde9054a5cf0ed83353ca7dcef88c2abe
Carlos Gabriel <carlosgabrielpc@live.com>
2021-12-19 14:04:18 -0300

    feat: add teacher router

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import coursesRouter from './routers/coursesRouter';
 import classesRouter from './routers/classesRouter';
+import teachersRouter from './routers/teachersRouter';
 
 const router: Router = Router();
 
 router.use('/courses', coursesRouter);
 router.use('/classes', classesRouter);
+router.use('/teachers', teachersRouter);
 
 export default router;
